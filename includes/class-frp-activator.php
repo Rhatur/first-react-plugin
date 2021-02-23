4303ce324f56eec19ed0d7b206e6758d651057ca
Arthur <arthur@ad-dev.me>
2021-02-23 10:08:34 +0000

    Added shortcode creation on plugin activation

--- includes/class-frp-activator.php
+++ includes/class-frp-activator.php
@@ -20,17 +20,23 @@
  * @subpackage Frp/includes
  * @author     Arthur <arthur@mbstudio.email>
  */
-class Frp_Activator {
+class Frp_Activator
+{
 
-	/**
-	 * Short Description. (use period)
-	 *
-	 * Long Description.
-	 *
-	 * @since    1.0.0
-	 */
-	public static function activate() {
-
-	}
-
+    /**
+     * Short Description. (use period)
+     *
+     * Long Description.
+     *
+     * @since    1.0.0
+     */
+    public static function activate()
+    {
+        $saved_page_args = array(
+            'post_title'   => __('React Testing on wordpress', 'mbs'),
+            'post_content' => '[mbs-react]',
+            'post_status'  => 'publish',
+            'post_type'    => 'page'
+        );
+    }
 }
